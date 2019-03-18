29644c5f52f687380dacf0d67be3e4070a875851
Savannah Milton <sjamz5555@gmail.com>
2019-03-18 22:03:16 +0000

    finish level 4

--- js/game.js
+++ js/game.js
@@ -6,41 +6,76 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You find a magic wand!",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Keep it!",
+                    nextLevel: "fairyCastle",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Walk away. Could be dangerous",
+                    nextLevel: "wizard",
                 },
             ]
         },
 
-        cave: {
+        fairyCastle: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You are transported to a fairy castle and are greeted by the Queen of Faries",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Threaten her and demand to be sent home",
+                    nextLevel: "fairyJail",
                 },
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        wizard: {
+            message: "A wizard appears and chalenges you to a duel beliving that you are his nemisis in disquise",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Fight him",
+                    nextLevel: "death",
                 },
+                {
+                  text:"Run Away",
+                  nextLevel:"mundaneLife",
+                },
+                
             ]
         },
-
+             death: {
+            message: "You tried to fight, but he burnt you to a crisp with his fire spell.",
+            choices: [
+                {
+                    text:"Start Over",
+                    nextLevel: "startover",
+                    
+                },
+                ]
+        },
+        fairyJail:{
+            message:"Once you threatened her, her guards seized your arms. You shall rot in fairy jail for your insults.",
+        choices:[
+            {
+                text:"Start Over",
+        nextLevel:"startover",
+            
+        },
+       ]
+    },
+    mundaneLife:{
+        message:"You live another",
+        choices:[
+            {
+                text:"Start over",
+            nextLevel:"startover",
+                
+            }
+            ]
+        
+    },
     }
 };
