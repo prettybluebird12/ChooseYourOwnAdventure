f643a058a6d8c3ccb36f4a8755689aa0d78f0783
Savannah Milton <sjamz5555@gmail.com>
2019-04-06 04:55:27 +0000

    logged all outcomes

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@ var game = {
     background_image: "intro-bg.jpg",
     levels: {
 
-        start: {
+        startover: {
             message: "You find a magic wand!",
             choices: [
                 {
@@ -29,11 +29,15 @@ var game = {
                     text: "Threaten her and demand to be sent home",
                     nextLevel: "fairyJail",
                 },
+                {
+                  text:"Ask to know secrets of the fairy realm",  
+                  nextLevel:"Quest",
+                },
             ]
         },
 
         wizard: {
-            message: "A wizard appears and chalenges you to a duel beliving that you are his nemisis in disquise",
+            message: "A wizard appears and challenges you to a duel beliving that you are his nemisis in disquise",
             choices: [
                 {
                     text: "Fight him",
@@ -56,8 +60,21 @@ var game = {
                 },
                 ]
         },
+        Quest:{
+            message:"The Queen says that in order to find learn the secrets you must go on a quest. Find her a dragon egg and you will become a fairy.",
+            choices:[
+                {
+                    text:"Take the quest",
+                    nextLevel:"thePath",
+                },
+                {
+                    text:"Sorry I don't feel like questing today",
+                nextLevel:"fairyJail",
+                },
+                ]
+        },
         fairyJail:{
-            message:"Once you threatened her, her guards seized your arms. You shall rot in fairy jail for your insults.",
+            message:"The Queen is angered by your ungratefulness, her guards seized your arms. *You shall rot in fairy jail for your insults* she says",
         choices:[
             {
                 text:"Start Over",
@@ -67,7 +84,7 @@ var game = {
        ]
     },
     mundaneLife:{
-        message:"You live another",
+        message:"You live another boring day.",
         choices:[
             {
                 text:"Start over",
@@ -77,5 +94,64 @@ var game = {
             ]
         
     },
+    thePath:{
+      message:"You have left the fairy castle on a horse galloping down the path. You come to a fork in the path. Which path dou you take?",
+     choices:[
+         {
+         text:"Path with dense forest that will take a week to cover",
+         nextLevel:"DenseForest",
+         },
+         {
+           text:"I choose the path with less dense forest that will take 3 days to cover",
+           nextLevel:"ClearForest",
+         },
+         ] 
+         },
+         DenseForest:{
+           message:"You travel for a week. You are sweaty tired and hungry. What do you do next?",
+           choices:[
+               {
+               text:"Kill the frog nearby to eat it",
+               nextLevel:"poison",
+               },
+               {
+                 text:"KEEP GOING!",
+                 nextLevel:"move",
+               },
+               ]
+         },
+         ClearForest:{
+         message:"You are attacked by a man eating plant.",
+         choices:[{
+             text:"The plant kills you",
+             nextLevel:"startover",
+         },]
+    },
+    move:{
+        message:"You find the dragon after another day of travel. How do you appaoach?",
+        choices:[{
+            text:"Attack with a yell",
+            nextLevel:"dragonDeath",
+        },
+        {
+          text:"Sneak up quietly",
+          choices:"ReturntoCatsle",
+        },]
+    },
+    ReturntoCatsle:{
+        message:"You return to the castle triumphantly and give the Queen the egg. In turn she blesses you with fairy wings.YOU WIN! and fly away on your new wings",
+        choices:[{
+            text:"Start Over",
+            nextLevel:"startover",
+        }]
+    },
+    dragonDeath:{
+    message:"Your screaming wakes the sleeping dragon and it attacks you burning your body You are dead.",
+    choices:[{
+    text:"Start Over",
+    nextLevel:"startover",
+    }]
+    },
+    
     }
 };
