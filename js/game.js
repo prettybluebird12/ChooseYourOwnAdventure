ec8d485c6689c76fdf9024e40a47fc30b3ed0fc3
Savannah Milton <sjamz5555@gmail.com>
2019-04-08 01:29:49 +0000

    done. i need to fix background.

--- js/game.js
+++ js/game.js
@@ -5,7 +5,8 @@ var game = {
     background_image: "intro-bg.jpg",
     levels: {
 
-        startover: {
+        start: {
+             background_image:"https://theoriginalpartybagcompany.co.uk/wp-content/uploads/2017/09/princess-fairy-wand-by-mimi-lula.jpg",
             message: "You find a magic wand!",
             choices: [
                 {
@@ -21,7 +22,7 @@ var game = {
         },
 
         fairyCastle: {
-            background_image: "fire.gif",
+    
             music: "Final-Fantasy-7-Boss-Battle.mp3",
             message: "You are transported to a fairy castle and are greeted by the Queen of Faries",
             choices: [
@@ -51,12 +52,12 @@ var game = {
             ]
         },
              death: {
+                 background_image:"fire.gif",
             message: "You tried to fight, but he burnt you to a crisp with his fire spell.",
             choices: [
                 {
                     text:"Start Over",
-                    nextLevel: "startover",
-                    
+                    nextLevel: "start",
                 },
                 ]
         },
@@ -69,89 +70,105 @@ var game = {
                 },
                 {
                     text:"Sorry I don't feel like questing today",
-                nextLevel:"fairyJail",
+                    nextLevel:"fairyJail",
                 },
                 ]
         },
         fairyJail:{
             message:"The Queen is angered by your ungratefulness, her guards seized your arms. *You shall rot in fairy jail for your insults* she says",
-        choices:[
-            {
-                text:"Start Over",
-        nextLevel:"startover",
+            choices:[
+                {
+                    text:"You are imprisoned",
+                    nextLevel:"start",
             
-        },
-       ]
-    },
-    mundaneLife:{
-        message:"You live another boring day.",
-        choices:[
-            {
-                text:"Start over",
-            nextLevel:"startover",
-                
-            }
+                },
             ]
-        
-    },
-    thePath:{
-      message:"You have left the fairy castle on a horse galloping down the path. You come to a fork in the path. Which path dou you take?",
-     choices:[
-         {
-         text:"Path with dense forest that will take a week to cover",
-         nextLevel:"DenseForest",
-         },
-         {
-           text:"I choose the path with less dense forest that will take 3 days to cover",
-           nextLevel:"ClearForest",
-         },
-         ] 
-         },
-         DenseForest:{
+        },
+        mundaneLife:{
+            message:"You live another boring day.",
+            choices:[
+                {
+                    text:"Start over",
+                    nextLevel:"start",
+                
+                },
+            ]
+        },
+        thePath:{
+            message:"You have left the fairy castle on a horse galloping down a path. You come to a fork in the path. Which path dou you take?",
+            choices:[
+                {
+                    text:"Path with dense forest that will take a week to cover",
+                    nextLevel:"DenseForest",
+                },
+                {
+                    text:"I choose the path with less dense forest that will take 3 days to cover",
+                    nextLevel:"ClearForest",
+                },
+           ] 
+        },
+        DenseForest:{
            message:"You travel for a week. You are sweaty tired and hungry. What do you do next?",
            choices:[
                {
-               text:"Kill the frog nearby to eat it",
-               nextLevel:"poison",
+                    text:"Kill the frog nearby to eat it",
+                    nextLevel:"poison",
                },
                {
-                 text:"KEEP GOING!",
-                 nextLevel:"move",
+                    text:"KEEP GOING!",
+                    nextLevel:"move",
                },
-               ]
-         },
-         ClearForest:{
-         message:"You are attacked by a man eating plant.",
-         choices:[{
-             text:"The plant kills you",
-             nextLevel:"startover",
-         },]
-    },
-    move:{
-        message:"You find the dragon after another day of travel. How do you appaoach?",
-        choices:[{
-            text:"Attack with a yell",
-            nextLevel:"dragonDeath",
+            ]
         },
-        {
-          text:"Sneak up quietly",
-          choices:"ReturntoCatsle",
-        },]
+        poison:{
+            message:"You eat the frog.",
+            choices:[
+                {
+                    text:"You are dead.",
+                    nextLevel:"start",
+                },
+            ]
+        },
+        move:{
+            message:"You find the dragon after another day of travel. How do you appaoach?",
+            choices:[
+            {
+                text:"Attack with a yell",
+                nextLevel:"dragonDeath",
+            },
+            {
+                text:"Sneak up quietly",
+                nextLevel:"ReturntoCastle",
+            },
+        
+            ]
+        },
+        ClearForest:{
+            message:"While walking around and admiring the beauty of the forest, you are attacked by a man-eating plant and you are eaten.",
+            choices:[
+                {
+                    text: "You are dead",
+                    nextLevel:"start",
+                }]
+        },
+        ReturntoCastle:{
+            message:"You return to the castle triumphantly and give the Queen the egg. In turn she blesses you with fairy wings. YOU WIN! and fly away on your new wings",
+            choices:[
+            {
+                text:"Start Over",
+                nextLevel:"start",
+            },
+            ]
+        },
+        dragonDeath:{
+            background_image:"fire.gif",
+            message:"Your screaming wakes the sleeping dragon and it attacks you burning your body.",
+            choices:[
+            {
+                text:"You are dead.",
+                nextLevel:"start",
+            },
+            ]
+        }
     },
-    ReturntoCatsle:{
-        message:"You return to the castle triumphantly and give the Queen the egg. In turn she blesses you with fairy wings.YOU WIN! and fly away on your new wings",
-        choices:[{
-            text:"Start Over",
-            nextLevel:"startover",
-        }]
-    },
-    dragonDeath:{
-    message:"Your screaming wakes the sleeping dragon and it attacks you burning your body You are dead.",
-    choices:[{
-    text:"Start Over",
-    nextLevel:"startover",
-    }]
-    },
-    
-    }
-};
+    };
